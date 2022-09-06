e0fe13175fc8628ba4d3e06afef8eec1f17cbe5d
ekubay <axuTec14@gmail.com>
2022-09-06 19:46:22 +0300

    testing file

---
+++ tests/test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from scripts import util
+
+class TestUtils(unittest.TestCase):
+  
+  def test_get_holiday(self):
+    self.assertEqual(util.get_holiday('a'), 'Public holiday')
+    self.assertEqual(util.get_holiday('b'), 'Easter holiday')
+    self.assertEqual(util.get_holiday('c'), 'Christmas')
+
+
+  def test_get_holiday_return_none(self):
+    self.assertEqual(util.get_holiday('0'), None)
+    self.assertEqual(util.get_holiday('e'), None)
+
+  def test_get_assortment(self):
+    self.assertEqual(util.get_assortment('a'), "Basic")
+    self.assertEqual(util.get_assortment('b'), "Extra")
+    self.assertEqual(util.get_assortment('c'), "Extended")
